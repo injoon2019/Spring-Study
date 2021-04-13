09d465f3e7920030a08003b426ef08d04f80e85a
Injun Son <ison@sfu.ca>
2021-04-13 22:39:42 +0900

    JPA 기본편 - 엔티티 매핑

---
+++ 인프런/자바 ORM 표준 JPA 프로그래밍 - 기본편/study/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -0,0 +1,26 @@
+package hellojpa;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
+    public Member() {
+    }
+}
